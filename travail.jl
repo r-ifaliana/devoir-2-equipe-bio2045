37ca36e58702c7f9e64abb0432cb305115a813a6
Dorra Ben Brahim <dorra.ben.brahim@umontreal.ca>
2026-03-11 14:03:44 -0400

    documentation pour les fct du code de base

--- travail.jl
+++ travail.jl
@@ -54,8 +54,16 @@ using Distributions
 import Random
 Random.seed!(2045)
 
+"""
+    check_transition_matrix!(T)
+
+Cette fonction vérifie que la somme de chaque ligne de 'T' est égale à 1, et si ce n'est pas le cas elle renvoi un warning pour signaler qu'elle fera des modifications sur l'objet 'T' pour faire en sorte que la somme de chache ligne soit égale à 1
+
+'T' doit être une matrice 
+"""
 function check_transition_matrix!(T)
-    for ligne in axes(T, 1)
+    # axes(T,1) retourne les indices de la premiere dimention de T donc les lignes
+    for ligne in axes(T, 1) 
         if sum(T[ligne, :]) != 1
             @warn "La somme de la ligne $(ligne) n'est pas égale à 1 et a été modifiée"
             T[ligne, :] ./= sum(T[ligne, :])
@@ -64,24 +72,51 @@ function check_transition_matrix!(T)
     return T
 end
 
+"""
+check_function_arguments(transitions, states)
+
+Cette fonction vérifie que la matrice de transition 'transition' est carrée donc que le nombre de ses lignes est égale au nombre de ses colonne et si ce n'est pas le cas elle arrête le programme et affiche un message d'erreur 
+elle vérifie aussi que le nombre de ligne de la matrice de transition est égale au nombre d'états possible et si ce n'est pas le cas elle arrête le programme et affiche un message d'erreur 
+
+'transitions' doit être une matrice de transition et 'states' doit être un vecteur avec les états possibles de la matrice de transition
+"""
 function check_function_arguments(transitions, states)
     if size(transitions, 1) != size(transitions, 2)
         throw("La matrice de transition n'est pas carrée")
     end
 
     if size(transitions, 1) != length(states)
-        throw("Le nombre d'états ne correspond psa à la matrice de transition")
+        throw("Le nombre d'états ne correspond pas à la matrice de transition")
     end
     return nothing
 end
 
+"""
+_sim_stochastic!(timeseries, transitions, generation)
+
+Cette fonction
+
+'timeseries' doit être une matrice
+'transitions' doit être 
+'generation' doit être le nombre de la génération dans laquelle on se trouve
+"""
 function _sim_stochastic!(timeseries, transitions, generation)
     for state in axes(timeseries, 1)
+        #Multinomial() cree échantillon aléa suivant une loi multinomiale (généralisation de la loi binomiale)
         pop_change = rand(Multinomial(timeseries[state, generation], transitions[state, :]))
         timeseries[:, generation+1] .+= pop_change
     end
 end
 
+"""
+_sim_determ!(timeseries, transitions, generation)
+
+Cette fonction
+
+'timeseries' doit être une matrice
+'transitions' doit être 
+'generation' doit être le nombre de la génération dans laquelle on se trouve
+"""
 function _sim_determ!(timeseries, transitions, generation)
     pop_change = (timeseries[:, generation]' * transitions)'
     timeseries[:, generation+1] .= pop_change
@@ -91,7 +126,8 @@ function simulation(transitions, states; generations=500, stochastic=false)
 
     check_transition_matrix!(transitions)
     check_function_arguments(transitions, states)
-
+    
+    #si determinist pop continue (car on calcule des proba) sinon pop entiere (car le vrai truc qui arrive)
     _data_type = stochastic ? Int64 : Float32
     timeseries = zeros(_data_type, length(states), generations + 1)
     timeseries[:, 1] = states
